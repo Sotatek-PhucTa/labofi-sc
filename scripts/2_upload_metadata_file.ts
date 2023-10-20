5357de44251399ef070a8e53039121c8a4f567f0
hoangphuc <hoangphucnb97@gmail.com>
2023-10-20 17:48:23 +0700

    Add function to upload metadata file

---
+++ scripts/2_upload_metadata_file.ts
@@ -0,0 +1,7 @@
+import {getWalletSuite, uploadMetadataFile} from "./helpers";
+
+(async () => {
+    const { metaplex } = await getWalletSuite("devnet", false);
+    const result = await uploadMetadataFile(metaplex, "LABOFI", "Labofi_Profile", "test_image.png");
+    console.log(result);
+})()
--- scripts/helpers.ts
+++ scripts/helpers.ts
@@ -1,9 +1,12 @@
 import * as anchor from "@coral-xyz/anchor";
 import { web3 } from "@coral-xyz/anchor";
+import {Metaplex, keypairIdentity, bundlrStorage, toMetaplexFile} from "@metaplex-foundation/js";
 import bs58 from "bs58";
 import dotenv from "dotenv";
 dotenv.config();
 import { LabofiSolanaSmartContract } from "../target/types/labofi_solana_smart_contract";
+import fs from "fs";
+import path from "path";
 
 export async function getWalletSuite(network: "devnet" | "mainnet", newWallet: boolean) {
     let keyPair: web3.Keypair = null;
@@ -15,17 +18,43 @@ export async function getWalletSuite(network: "devnet" | "mainnet", newWallet: b
     }
     const wallet = new anchor.Wallet(keyPair);
     let url = "";
+    let bundlrAddr = "";
     if (network === "devnet") {
         url = process.env.DEVNET_URL || "https://api.devnet.solana.com";
+        bundlrAddr = "https://devnet.bundlr.network";
     } else if (network === "mainnet") {
         url = process.env.MAINNET_URL || "https://api.mainnet.solana.com";
+        bundlrAddr = "https://mainnet.bundlr.network";
     }
     const connection = new web3.Connection(url);
     const provider = new anchor.AnchorProvider(connection, new anchor.Wallet(keyPair), { commitment: "confirmed" });
-    return { provider, wallet, keyPair };
+    const metaplex = Metaplex.make(connection)
+        .use(keypairIdentity(keyPair))
+        .use(bundlrStorage({
+            address: bundlrAddr,
+            providerUrl: url,
+            timeout: 60000,
+        }));
+    return { provider, wallet, keyPair, connection, metaplex };
 }
 
 export function getLabofiProgram(provider: anchor.Provider) {
     anchor.setProvider(provider);
     return anchor.workspace.LabofiSolanaSmartContract as anchor.Program<LabofiSolanaSmartContract>;
+}
+
+export async function uploadMetadataFile(metaplex: Metaplex, nftTitle: string, nftSymbol: string, imgName: string) {
+    const imagePath = path.join(__dirname, "../assets/images", imgName);
+    const image = fs.readFileSync(imagePath);
+    return await metaplex.nfts().uploadMetadata({
+        name: nftTitle,
+        symbol: nftSymbol,
+        image: toMetaplexFile(image, "image.png"),
+        attributes: [
+            {
+                trait_type: "rank",
+                value: "bronze",
+            }
+        ]
+    })
 }
